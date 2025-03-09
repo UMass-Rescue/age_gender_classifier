5b1365bfba22e40f6da7640377d56913d6501c01
james <james.bardowski@gmail.com>
2025-03-09 16:03:17 -0400

    feat: function to write df to db + gender plot

--- src/utils/common.py
+++ src/utils/common.py
@@ -23,3 +23,16 @@ def read_db(
 
     logging.info(f" Retrieved {len(df)} records from DB table {table_name}")
     return df
+
+
+def write_db(
+        df: pd.DataFrame,
+        table_name: str="age_gender_labeled"
+    ) -> None:
+    """Write pandas DataFrame into DB table."""
+
+    db = DBManager(db_uri, table_name)
+    df.to_sql(table_name, con=db.engine)
+    db.commit_and_close()
+
+    logging.info(f" Wrote {len(df)} records from DB table {table_name}")
