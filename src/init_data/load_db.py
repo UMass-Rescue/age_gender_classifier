af23d13e651d98c48c352fb40a9e125e8a15ebc5
Juhi Manish Jain <97155012+juhimanishjain@users.noreply.github.com>
2025-03-31 22:28:51 +0530

    modified code for dataset cleaning for image preprocessing

--- src/init_data/load_db.py
+++ src/init_data/load_db.py
@@ -1,11 +1,11 @@
-import os
+import os, numpy as np
 from pathlib import Path
 import logging
 import pandas as pd
 from src.utils.sqlAlchemy_manager import DBManager
+from src.utils.preprocess import is_valid_image, is_damaged
 
 logging.basicConfig(level=logging.INFO)
-
 
 def read_csv_and_format(fileIn: str="age_gender.csv") -> pd.DataFrame:
     "Read labeled data set and return DataFrame"
@@ -15,20 +15,21 @@ def read_csv_and_format(fileIn: str="age_gender.csv") -> pd.DataFrame:
         raise FileNotFoundError(f" File not found: {file}")
     return pd.read_csv(file)
 
-
 def clean_load_to_db(df: pd.DataFrame, table_name: str="age_gender_labeled") -> None:
     """Connect to DB, create/truncate table, write df to table."""
+    # cleaning pixels column for image preprocessing
+    df = df[df["pixels"].apply(is_valid_image)].copy()
+    df["pixels_array"] = df["pixels"].apply(lambda x: np.array([int(p) for p in x.split()], dtype=np.uint8).reshape(48, 48))
+    df = df[~df["pixels_array"].apply(is_damaged)]
+    df = df.drop(columns=["pixels_array"])
+    # Load to DB
     db_uri = os.getenv("DB_CONN_STR")
     db = DBManager(db_uri, table_name)
-
     db.create_table_if_not_exists()
     db.truncate_table()
-
     df.to_sql(table_name, con=db.engine, if_exists="append", index=False)
-
     ct = pd.read_sql(f"SELECT COUNT(*) FROM {table_name}", con=db.engine).iloc[0, 0]
     logging.info(f" Clean loaded {ct} records into DB table {table_name}")
-
 
 def main() -> None:
     df = read_csv_and_format()
